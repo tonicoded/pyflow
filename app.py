0ca81ff28f660fb13cfac7d5316509fe260b9ba6
tonicoded <anthonyvvza@gmail.com>
2025-04-05 13:57:11 +0200

    f

--- app.py
+++ app.py
@@ -91,9 +91,6 @@ def blog():
 @app.route('/blog/tijdbesparen-met-automatisering')
 def blog_tijdbesparen():
     return render_template("blog_tijdbesparen.html", time=int(time.time()))
-@app.route('/automatiseringcheck')
-def automatiseringcheck():
-    return render_template('automatiseringcheck.html', time=int(time.time()))
 
 @app.route('/prijsindicatie')
 def prijsindicatie():
